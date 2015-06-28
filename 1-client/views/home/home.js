b265ce46356720746fd7a01f853231506fefcaf4
Alex Liu <yuningalexliu@gmail.com>
2015-06-27 22:24:14 -0400

    incorporate sequencer frontend and get angular to work

--- 1-client/views/home/home.js
+++ 1-client/views/home/home.js
@@ -1,4 +1,65 @@
+app.controller('SequenceController', function ($scope) {
 
-// angular.module('myApp.controllers').controller('homeCtrl', ['$scope', '$http', function($scope, $http) {}]);
+	function Sequencer () {
+		this.sequence = [];
+		this.push = function(block) {
+			this.sequence.push(block);
+		};
+		// this.pop = function() {
+		// 	this.sequence.pop();
+		// }
 
-scope.myvar = "Hello!";
+		this.run = function() {
+			$scope.startBlock.execute();
+			for (var i = 0; i < this.sequence.length; i++) {
+				if (i === 0) {
+					this.sequence[i].setInput($scope.startBlock.output);
+					this.sequence[i].execute();
+				} else {
+					this.sequence[i].setInput(this.sequence[i-1].output);
+					this.sequence[i].execute();
+				}
+				console.log("Ran a block!");
+			}
+			// console.log(this.sequence[this.sequence.length-1].output);
+		}
+	}
+
+	function StartBlock () {
+		this.input;
+		this.funct;
+		this.language;
+		this.execute = function() {
+			this.output = eval("(" + this.funct + ")" + "(" + this.input + ")");
+
+			// Write the function to a file with appropriate readFile and writeFile methods in correct language
+			// fs.writeFile('files')
+			// 
+		};
+	}
+
+	function Block () {
+		this.input;
+		this.funct;
+		this.setInput = function(input) {
+			this.input = input;
+		}
+		this.execute = function() {
+			this.output = eval("(" + this.funct + ")" + "(" + this.input + ")");
+		};
+	}
+
+	$scope.sequencer = new Sequencer();
+	$scope.startBlock = new StartBlock();
+
+    $scope.createBlock = function() {
+    	var block = new Block();
+    	$scope.sequencer.push(block);
+    	console.log("New Block created!");
+    }
+
+    $scope.runSequence = function() {
+    	$scope.sequencer.run();
+    }
+
+});
