209b66eba5117a7c00cf4d37e00c74fcb1cf1353
Alex Liu <yuningalexliu@gmail.com>
2015-07-03 22:59:41 -0400

    got ruby container working

--- 2-server/APIs/scriptwriter.js
+++ 2-server/APIs/scriptwriter.js
@@ -12,47 +12,76 @@ router.get('/', function (req, res) {
 
 	var languageToFileExtensionMapping = {
 		'node' : '.js',
-		'python' : '.py'
+		'ruby' : '.rb'
 	}
 
 	// NOTE: THIS IS UNIQUE FOR EACH DOCKER RUNNING ENVIRONMENT
 	var dockerImageId = {
-		'node' : 'b8844354c16f'
+		// Alex's node image mapping
+		'node' : 'b8844354c16f',
+
+		// Greg's node image mapping
 		// 'node' : '4797dc6f7a9c'
+
+		// Alex's ruby image mapping
+		'ruby' : '587d0d048bce'
+
+		// Greg's ruby image mapping
 	}
 
 	console.log(req.query);
 
 	var language = req.query.language;
+	console.log("The language is", language);
 	var input_type = req.query.input_type;
-	console.log("the input_type is:", input_type)
+	console.log("the input_type is:", input_type);
 	var fileExtension = languageToFileExtensionMapping[language]
 	var blockPosition = req.query.blockPosition;
-	var scriptName = blockPosition + fileExtension
-
+	var scriptName = blockPosition + fileExtension;
+	var scriptContent;
 	var parser;
 
-	(function(inp){
-		if(inp === 'string'){parser = ""}
-		else if(inp === 'integer'){parser = "input = Number(input)"}
-	})(input_type);
+	if (language === "node") {	
+		(function(inp){
+			if(inp === 'string'){parser = ""}
+			else if(inp === 'integer'){parser = "input = Number(input)"}
+		})(input_type);
+	} else if (language === "ruby") {
+		parser = "";
+	}
 
 	console.log("parser: ",parser)
 
-	var scriptContent = "fs = require('fs'); \
-	var myFunc = function(infile, outfile){ \
-		fs.readFile(infile, function(err, input){ \
-			if(err){console.log(err)}; \
-			 " + parser + "; \
-			 var result = function (input) {" + req.query.funct + "}(input); \
-			 fs.writeFile(outfile, result, function (err) { \
-			 	if (err) return console.log(err); \
-			 	console.log('output written: ' + outfile); \
-			 	fs.writeFile('6-docker-done/done.json', 'done', function (err) {if (err) {console.log(err)}; console.log('DONE WRITING done.json');}); \
-			 }); \
-		}); \
-	}; \
-	myFunc(process.argv[2], process.argv[3]);" 
+	if (language === "node") {
+		console.log("USING NODE SCRIPT!");
+		scriptContent = "fs = require('fs'); \
+		var myFunc = function(infile, outfile){ \
+			fs.readFile(infile, function(err, input){ \
+				if(err){console.log(err)}; \
+				 " + parser + "; \
+				 var result = function (input) {" + req.query.funct + "}(input); \
+				 fs.writeFile(outfile, result, function (err) { \
+				 	if (err) return console.log(err); \
+				 	console.log('output written: ' + outfile); \
+				 	fs.writeFile('6-docker-done/done.json', 'done', function (err) {if (err) {console.log(err)}; console.log('DONE WRITING done.json');}); \
+				 }); \
+			}); \
+		}; \
+		myFunc(process.argv[2], process.argv[3]);" 
+	} else if (language === "ruby") {
+		console.log("USING RUBY SCRIPT!");
+		scriptContent = " \
+			def myFunc(pathToInputFile, pathToOutputFile); \
+				input = File.read(pathToInputFile); \
+				def func(input); \
+					" + req.query.funct + "; \
+				end; \
+				output = func(input); \
+				File.open(pathToOutputFile, 'w') { |file| file.write(output) }; \
+				File.open('6-docker-done/done.json', 'w') { |file| file.write('done') }; \
+			end; \
+			myFunc(ARGV.first, ARGV[1]);"
+	}
 
 	var image = dockerImageId[language];
 	console.log("imageId: ", image)
@@ -64,6 +93,7 @@ router.get('/', function (req, res) {
 
 	async.series([
 		function(callback){
+			console.log("About to write script content of", scriptContent);
 		    fs.writeFile('3-scripts/' + scriptName, scriptContent, function(err){
 		    	if (err) console.log(err);
 		    	console.log('scriptfile saved.');
@@ -115,8 +145,10 @@ router.get('/', function (req, res) {
 			  	}, 2000);
 			  });
 
+			console.log("The LANGUAGE IS", language);
+			console.log("The IMAGE IS", image);
 			console.log('Sending stdin to terminal');
-			console.log('Running: ' + 'docker run -d -v "$PWD":/src/app -w /src/app ' + image + ' ' + language + ' ' + '3-scripts/'+scriptName + ' ' + '4-data/'+infile + ' ' + '4-data/'+outfile + '\n')
+			console.log('RUNNING: ' + 'docker run -d -v "$PWD":/src/app -w /src/app ' + image + ' ' + language + ' ' + '3-scripts/'+scriptName + ' ' + '4-data/'+infile + ' ' + '4-data/'+outfile + '\n')
 			terminal.stdin.write('docker run -d -v "$PWD":/src/app -w /src/app ' + image + ' ' + language + ' ' + '3-scripts/'+scriptName + ' ' + '4-data/'+infile + ' ' + '4-data/'+outfile + '\n');
 
 		},
